5838e8648627a4778ad525c49938bdf873fda68f
Yasuhito Takahata <highvoltg@YasuhitonoMacBook-Air.local>
2025-01-18 20:05:08 +0900

    Initial commit

---
+++ playground/script.js
@@ -0,0 +1,76 @@
+dataLayer.push({
+    event: "view_item",
+    eventModel: {
+      ecomm_prodid: ["shopify_JP_7575842422936_42491717583000"],
+      ecomm_totalvalue: null,
+      ecomm_pagetype: "product",
+      items: [
+        {
+          id: "shopify_JP_7575842422936_42491717583000",
+          name: "テスト",
+          brand: "Ralph Lauren",
+          category: "ファッション雑貨",
+          price: "0",
+          variant: null
+        }
+      ]
+    },
+    gtm.uniqueEventId: 12
+  })
+
+  dataLayer.push({
+    event: "add_to_cart",
+    eventModel: {
+      ecomm_prodid: ["shopify_JP_7575842422936_42491717583000"],
+      ecomm_totalvalue: 0,
+      ecomm_pagetype: "cart",
+      value: 0,
+      currency: "JPY",
+      items: [
+        {
+          id: "shopify_JP_7575842422936_42491717583000",
+          name: "テスト",
+          brand: "Ralph Lauren",
+          category: "ファッション雑貨",
+          price: "0",
+          quantity: "1",
+          variant: null
+        }
+      ]
+    },
+    gtm.uniqueEventId: 20
+  })
+
+  
+0: "event"
+1: "view_item"
+2: ecomm_pagetype: "product"
+ecomm_prodid:['shopify_JP_7575842422936_42491717583000']
+ecomm_totalvalue:NaN
+items:Array(1)
+0: brand:"Ralph Lauren"
+category:"ファッション雑貨"
+coupon:undefined
+id:"shopify_JP_7575842422936_42491717583000"
+name:"テスト"
+price:"0"
+quantity:undefined
+variant:null
+
+
+0:"event"
+1:"add_to_cart"
+2:currency:"JPY"
+ecomm_pagetype:"cart"
+ecomm_prodid:['shopify_JP_7575842422936_42491717583000']
+ecomm_totalvalue:0
+items:Array(1)
+
+brand:"Ralph Lauren"
+category:"ファッション雑貨"
+coupon:undefined
+id:"shopify_JP_7575842422936_42491717583000"
+name:"テスト"
+price:"0"
+quantity:"1"
+variant:null
